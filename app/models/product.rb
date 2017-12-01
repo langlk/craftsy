e7c3fbade80a24a9a4a4fe5fbfbe2c6282a7fd8f
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-01 14:37:04 -0800

    Add scope for most recent Products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   has_many :reviews
   validates_presence_of :name, :description, :price
   validates :price, numericality: { greater_than_or_equal_to: 1 }
+
+  scope :most_recent, -> { order("created_at DESC").limit(3) }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,14 @@ describe Product do
   it { should validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
 
   it { should have_many :reviews }
+
+  describe '.most_recent' do
+    it "returns the three most recently created products" do
+      p1 = FactoryBot.create(:product)
+      p2 = FactoryBot.create(:product)
+      p3 = FactoryBot.create(:product)
+      p4 = FactoryBot.create(:product)
+      expect(Product.most_recent).to eq([p4, p3, p2])
+    end
+  end
 end
