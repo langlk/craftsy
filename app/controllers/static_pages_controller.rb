23ab4dae36708dc7c652fe9b67cb1bd096e8b3c2
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-01 14:43:32 -0800

    Add recent products to landing page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:landing]
   def landing
-    render :landing
+    @products = Product.most_recent
   end
 end
