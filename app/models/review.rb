5487d8a8a06038ecd0ff214349ead4e557c294d0
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-01 14:32:37 -0800

    Update review to validate rating range.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
   validates_presence_of :user, :product, :comments, :rating
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,6 +5,10 @@ describe Review do
   it { should validate_presence_of :product }
   it { should validate_presence_of :comments }
   it { should validate_presence_of :rating }
+  it { should validate_numericality_of(:rating)
+    .is_greater_than_or_equal_to(1)
+    .is_less_than_or_equal_to(5)
+  }
 
   it { should belong_to :user }
   it { should belong_to :product }
