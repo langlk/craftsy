f2a6d2cec0cb427ee2aa72ea750aa8a0da4169af
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-01 09:47:33 -0800

    Make usernames unique.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :name 
+  validates_presence_of :name
+  validates_uniqueness_of :name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
