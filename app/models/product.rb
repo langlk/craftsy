b0772ee106f6fc9de51be7da6c0b5fc8a4570c05
Kelsey Langlois <kels.langlois@gmail.com>
2017-12-01 14:29:49 -0800

    Update product to validate price range

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   validates_presence_of :name, :description, :price
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
   it { should validate_presence_of :price }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
 
   it { should have_many :reviews }
 end
